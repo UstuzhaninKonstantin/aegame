597f060d007f265ebf95976b5caa1e8ac457d5dd
UstuzhaninKonstantin <themad.jr@gmail.com>
2023-06-29 10:42:39 +0300

    respawn

--- src/entities.js
+++ src/entities.js
@@ -1,5 +1,31 @@
-export const entities = [];
+import { canvas } from "./canvas.js";
+import { Enemy } from "./entities/Enemy.js";
+import { Player } from "./entities/Player.js";
+
+export let entities = [];
+export let player;
 
 export function addEntity(entity) {
     entities.push(entity);
+}
+
+export function setPlayer(p) {
+    player = p;
+    entities.push(p);
+}
+
+export function createEntities() {
+    const player = new Player(100, canvas.height / 2, 50, 50, "#df134a", 10, 10);
+    setPlayer(player);
+    addEntity(new Enemy(1600, 100, 20, 20, "#123456"));
+}
+
+export function deleteEntities() {
+    entities = [];
+    player = undefined;
+}
+
+export function restart() {
+    deleteEntities();
+    createEntities();
 }
