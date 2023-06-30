2fa44daa42a9fb013c5009e2449603ac34768cdb
UstuzhaninKonstantin <themad.jr@gmail.com>
2023-06-30 14:53:58 +0300

    menu

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import { run } from "./run.js";
 
-run();
+const playButton = document.getElementById("play");
+playButton.addEventListener("click", () => {
+    const canvas = document.getElementById("canvas");
+    const menu = document.getElementById("menu");
+    canvas.style.display = "block";
+    menu.style.display = "none";
+    run();
+});
